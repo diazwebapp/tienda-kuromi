3c505b6ff3e3b15bbb20a34be0ae1bce1e5eb926
diaz web app <diazwebapp@gmail.com>
2025-07-05 18:14:14 +0000

    iniciando la estructura

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL,
+  integrations: [sitemap()],
+  image:{
+    domains:['m.amazon.com']
+  }
+});
