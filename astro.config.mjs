44ab6b6344923c120771ced3e28e12211d5ecb47
diaz web app <diazwebapp@gmail.com>
2025-07-09 20:19:15 +0000

    no carga la imagen, se añadio el dominio en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   site: process.env.PUBLIC_SITE_URL,
   integrations: [sitemap()],
   image:{
-    domains:['m.media-amazon.com','i.ebayimg.com']
+    domains:['m.media-amazon.com','i.ebayimg.com','tienda-kuromi.vercel.app']
   }
 });
