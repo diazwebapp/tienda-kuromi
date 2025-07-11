f90624f9757ea6948f0779cc9f08ddc2a3f51304
diaz web app <diazwebapp@gmail.com>
2025-07-11 16:41:37 +0000

    se añadio funko pop a juguetes

--- src/data/juguetes.ts
+++ src/data/juguetes.ts
@@ -156,3 +156,140 @@ export const peluches:Peluche[]=[
                 "refLink":"https://www.ebay.es/itm/226281831247?_skw=peluche+kuromi+50cm"
             }
         ]
+
+type Funko={
+        nombre:string
+        precio:number
+        iamgen:string
+        rating:number
+        color:string
+        edicion:number
+        ratingCount:number
+        tienda: "Amazon" | "ebay"
+        refLink?: string
+    }
+
+    export const funkos:Funko[]=[
+        {
+            nombre:"Funko Pop Kuromi 90",
+            precio:16.00,
+            iamgen:"https://m.media-amazon.com/images/I/61z4sIi+S6L._AC_SX679_.jpg",
+            rating:4.3,
+            ratingCount:396,
+            edicion:90,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/4kAshcP"
+        },
+        {
+            nombre:"Funko Pop melody 91",
+            precio:16.00,
+            iamgen:"https://m.media-amazon.com/images/I/611ndYUan7L._AC_SX679_.jpg",
+            rating:4.3,
+            ratingCount:276,
+            edicion:91,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/3TCJzei"
+        },
+        {
+            nombre:"Funko Pop Cinnamoroll 92",
+            precio:16.00,
+            iamgen:"https://m.media-amazon.com/images/I/71NPyvsAsdL._AC_SX679_.jpg",
+            rating:4.3,
+            ratingCount:316,
+            edicion:92,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/4lOsgCY"
+        },
+        {
+            nombre:"Funko Pop Kuromi 101",
+            precio:16.00,
+            iamgen:"https://m.media-amazon.com/images/I/61A3OzCjq0L._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:47,
+            edicion:101,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/4lI4yZ6"
+        },
+        {
+            nombre:"Funko Pop Kuromi 108",
+            precio:18.00,
+            iamgen:"https://m.media-amazon.com/images/I/61qpgAZCs2L._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:47,
+            edicion:108,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/3GHklZj"
+        },
+        {
+            nombre:"Funko Pop Kuromi 105",
+            precio:23.00,
+            iamgen:"https://m.media-amazon.com/images/I/51AhQxbCcvL._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:1,
+            edicion:105,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/44pvuqR"
+        },
+        {
+            nombre:"Funko Pop Kuromi premium",
+            precio:23.00,
+            iamgen:"https://m.media-amazon.com/images/I/71QhdKRvvJL._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:18,
+            edicion:0,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/4kIY4sg"
+        },
+        {
+            nombre:"figura Kuromi premium",
+            precio:19.00,
+            iamgen:"https://m.media-amazon.com/images/I/51fa0NC1raL._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:18,
+            edicion:0,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/4lS69f9"
+        },
+        {
+            nombre:"Funko Pop melody 103",
+            precio:185.00,
+            iamgen:"https://m.media-amazon.com/images/I/61WBhCiCnrL._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:52,
+            edicion:103,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/3GKlNKm"
+        },
+        {
+            nombre:"Funko Pop Cinnamoroll 100",
+            precio:185.00,
+            iamgen:"https://m.media-amazon.com/images/I/61xZclTXN7L._AC_SX679_.jpg",
+            rating:5,
+            ratingCount:83,
+            edicion:100,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/4eT65JO"
+        },
+        {
+            nombre:"Funko Pop! 62: Hello Kitty - Fiesta del Unicornio ",
+            precio:185.00,
+            iamgen:"https://m.media-amazon.com/images/I/71zCDHKt7eL._AC_SY879_.jpg",
+            rating:5,
+            ratingCount:1027,
+            edicion:62,
+            color:"multicolor",
+            tienda:"Amazon",
+            refLink:"https://amzn.to/40NR85N"
+        }
+        
+    ]
